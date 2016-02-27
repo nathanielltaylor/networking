80a5d3f32c6780b816a3fb84f14b4789a44f07e8
nathanielltaylor <ntaylor390@gmail.com>
2016-02-26 22:14:53 -0500

    client handler

--- netcat_clone.py
+++ netcat_clone.py
@@ -70,6 +70,41 @@ def run_command(command):
         output = "Failed to execute command\r\n"
     return output
 
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+
+    if len(upload_destination):
+        file_buffer = ""
+
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+        try:
+            file_descriptor = open(upload_destination, "wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+            client_socket.send("Successfully saved file to %s\r\r" % upload_destination)
+        except:
+            client_socket.send("Failed to save file to %s\r\r" % upload_destination)
+
+    if len(execute):
+        output = run_command(execute)
+        client_socket.send(output)
+
+    if command:
+        while True:
+            client_socket.send("<NCC#> ")
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+            response = run_command(cmd_buffer)
+            client_socket.send(response)
+
 def main():
     global listen
     global port
