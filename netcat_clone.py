95fbd7f494711f8f4bb525ef383c810cfe5227ab
nathanielltaylor <ntaylor390@gmail.com>
2016-02-27 16:01:23 -0500

    ncc fixed

--- netcat_clone.py
+++ netcat_clone.py
@@ -4,63 +4,9 @@ import getopt
 import threading
 import subprocess
 
-listen, command, upload = (False,)*3
-execute, target, upload_destination = "", "", ""
+listen, command, upload = False,False,False
+execute, target, upload_destination = "","",""
 port = 0
-
-def usage():
-    print "Custom Netcat Tool\r\n"
-    print "Usage: netcat_clone.py -t target_host -p target_port"
-    print "-l --listen                    Listen on [host]:[port] for incoming connections"
-    print "-e --execute=file_to_run       Execute the given file upon receiving a connection"
-    print "-c --command                   Initialize a command shell"
-    print "-u --upload=destination        Upon receiving connection upload a file and write to [destination]"
-    print "\r\n\r\n"
-    print "Examples:"
-    print "netcat_clone.py -t 192.168.0.1 -p 5555 -l -c"
-    # print "netcat_clone.py"
-    sys.exit(0)
-
-def client_sender(buffer):
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    try:
-        client.connect((target_host,target_port))
-        if len(buffer):
-            client.send(buffer)
-        while True:
-            recv_len = 1
-            response = ""
-
-            while recv_len:
-                data = client.recv(4096)
-                recv_len = len(data)
-                response += data
-                if recv_len < 4096:
-                    break
-
-            print response,
-
-            buffer = raw_input("")
-            buffer += "\n"
-            client.send(buffer)
-    except:
-        print "=> Exception, exiting."
-        client.close()
-
-def server_loop():
-    global target
-
-    if not len(target):
-        target = "0.0.0.0"
-
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.bind((target,port))
-    server.listen(5)
-
-    while True:
-        client_socket, addr = server.accept()
-        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
-        client_thread.start()
 
 def run_command(command):
     command = command.rstrip()
@@ -104,6 +50,62 @@ def client_handler(client_socket):
                 cmd_buffer += client_socket.recv(1024)
             response = run_command(cmd_buffer)
             client_socket.send(response)
+
+def server_loop():
+    global target
+
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((target,port))
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
+        client_thread.start()
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect((target,port))
+        if len(buffer):
+            client.send(buffer)
+        while True:
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+                if recv_len < 4096:
+                    break
+
+            print response,
+
+            buffer = raw_input("")
+            buffer += "\n"
+            client.send(buffer)
+    except:
+        print "=> Exception, exiting."
+        client.close()
+
+def usage():
+    print "Custom Netcat Tool\r\n"
+    print "Usage: netcat_clone.py -t target_host -p target_port"
+    print "-l --listen                    Listen on [host]:[port] for incoming connections"
+    print "-e --execute=file_to_run       Execute the given file upon receiving a connection"
+    print "-c --command                   Initialize a command shell"
+    print "-u --upload=destination        Upon receiving connection upload a file and write to [destination]"
+    print "\r\n\r\n"
+    print "Examples:"
+    print "netcat_clone.py -t 192.168.0.1 -p 5555 -l -c"
+    print "netcat_clone.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
+    print "netcat_clone.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
+    print "echo 'message' | python netcat_clone.py -t 192.168.11.12 -p 135"
+    sys.exit(0)
 
 def main():
     global listen
