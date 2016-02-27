333d0d7908f14242cc642cd9cc0798943405ba86
nathanielltaylor <ntaylor390@gmail.com>
2016-02-26 19:51:54 -0500

    sending running looping

--- netcat_clone.py
+++ netcat_clone.py
@@ -18,8 +18,57 @@ def usage():
     print "\r\n\r\n"
     print "Examples:"
     print "netcat_clone.py -t 192.168.0.1 -p 5555 -l -c"
-    print "netcat_clone.py"
+    # print "netcat_clone.py"
     sys.exit(0)
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        client.connect((target_host,target_port))
+        if len(buffer):
+            client.send(buffer)
+        while True:
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+                if recv_len < 4096:
+                    break
+
+            print response,
+
+            buffer = raw_input("")
+            buffer += "\n"
+            client.send(buffer)
+    except:
+        print "=> Exception, exiting."
+        client.close()
+
+def server_loop():
+    global target
+
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((target,port))
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
+        client_thread.start()
+
+def run_command(command):
+    command = command.rstrip()
+    try:
+        output = subprocess.check_output(command,stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = "Failed to execute command\r\n"
+    return output
 
 def main():
     global listen
